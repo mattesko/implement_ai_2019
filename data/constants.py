10072f697a13e3ab75a706e894adf87c396ba6ab
Tristan Toupin <tristantoupin@gmail.com>
2019-09-28 16:06:44 -0400

    working model preds

--- data/constants.py
+++ data/constants.py
@@ -1,2 +1,13 @@
-sexy_words_list = [""]
+sexy_words_list = ["sexual", "sex", "homosexual", "homosexuality", "sexuality", "sexually", "essex", "bisexual",
+                   "heterosexual", "penis", "anal", "masturbation", "intercourse", "pregnancy", "reproductive",
+                   "sexually", "incest", "rape", "sexy", "porn", "incest", "rapecensorship","lesbian","gay",
+                   "pornographic","anime"]
 
+violent_words_list = ["war", "attact", "punch", "robber", "knife", "gun", "hit", "slap", "shove",
+                      "grab", "pinching", "biting", "rape", "forcing", "intimidation", "destruction",
+                      "name-calling", "damaging", "threatening", "death", "kill", "killing", "murder",
+                      "harm", "beating", "terrorist", "Warfare", "violence", "lacerate", "ravage",
+                      "decimate", "sabotage", "Alert", "Ambush", "Ammunition", "terrorists","terrorism",
+                      "attacks","bombing","qaeda","attack"]
+
+non_prod_words_list = ["game", "sex", "movie", "video", "playing", "dancing"]
