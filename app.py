6837e4900b86fdd87deab1da3e0a7f69089e2809
unknown <fournier.phk@gmail.com>
2019-09-29 01:13:18 -0400

    google api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, Response, jsonify
 from bs4 import BeautifulSoup
-from filters_utils import get_filter_value
+# from filters_utils import get_filter_value
 from functools import reduce
+from google_vision import google_visions
+import ast
 
 app = Flask(__name__)
 
@@ -14,7 +16,7 @@ def default():
 @app.route('/filter')
 def handle_filter():
     sentences = _parse_html_sentences(request.args['block'])
-    filters = [get_filter_value(sent) for sent in sentences]
+    # filters = [get_filter_value(sent) for sent in sentences]
     do_censor = reduce(lambda x, y: x and y, filters)
 
     for sentence, do_filter in zip(sentences, filters):
@@ -26,6 +28,22 @@ def handle_filter():
 
     return jsonify(content)
 
+
+@app.route('/img')
+def handle_images():
+    """
+    Function that parses the url input and does Google API inference on them.
+    NOTE: THE URLS PASSED IN MUST NOT HAVE '&' CHARACTER. THOSE SHOULD BE REPLACED BY '%26' 
+    :return: list of booleans for whether to obfuscate (True) or not (False).
+    """
+    # Get the list of urls that is passed in through the endpoint
+    sentences = _parse_html_urls(request.args['block'])
+    # Convert the string into a list of urls
+    sentences = ast.literal_eval(sentences.split()[0])
+    # Send list of urls
+    temp = google_visions(sentences)
+    # return the boolean values
+    return str(temp)
 
 def _parse_html_sentences(content):
     errors = []
@@ -44,6 +62,10 @@ def _parse_html_sentences(content):
     return sentences
 
 
+def _parse_html_urls(sentences):
+    for i in sentences:
+        print(i)
+    return sentences
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
-
